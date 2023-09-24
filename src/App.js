e1953aa6623e5f5e4005ed49b2ea59f9bd06ff1d
abdullah-25 <imv.abdullah@mail.utoronto.ca>
2023-09-24 14:59:05 -0400

    bootstrap install

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 function App() {
   return (
-    <div className="App">
-      <div>Hi!!!!!</div>
+    <div className="App container mt-5">
+      <h1 className="text-primary">
+        <i className="bi bi-diagram-2-fill"></i> Repos
+      </h1>
     </div>
   );
 }
