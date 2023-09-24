7c17736dbb093b3065b12bb30c3ee3fc074fce6f
abdullah-25 <imv.abdullah@mail.utoronto.ca>
2023-09-24 18:28:12 -0400

    add github token in db.js, send a post request to retrive name on app.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,32 @@
+import axios from "axios";
+import Github from "./db";
+import { useEffect, useState } from "react";
+
 function App() {
+  const [userName, setUserName] = useState("");
+
+  useEffect(() => {
+    const githubQuery = {
+      query: `{
+        viewer{
+          name
+        }
+      }`,
+    };
+    axios
+      .post(Github.baseURL, githubQuery, { headers: Github.headers })
+      .then((response) => {
+        setUserName(response.data.data.viewer.name);
+      })
+      .catch((err) => console.log(err));
+  }, []);
+
   return (
     <div className="App container mt-5">
       <h1 className="text-primary">
         <i className="bi bi-diagram-2-fill"></i> Repos
       </h1>
+      <p>Hi there {userName}</p>
     </div>
   );
 }
---
+++ src/db.js
@@ -0,0 +1,10 @@
+const Github = {
+  baseURL: "https://api.github.com/graphql",
+  username: "abdullah-25",
+  headers: {
+    "Content-Type": "application/json",
+    Authorization: "bearer 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",
+  },
+};
+
+export default Github;
